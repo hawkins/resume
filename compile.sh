8052524d62b2c3e09f4c7a374190ffd3fe62cfab
Josh Hawkins <jkhaccounts@gmail.com>
2017-10-03 12:31:43 -0500

    Improve compile.sh to rename file with date
    Also corrected gitignore, oops
    
    Fixes #1

--- compile.sh
+++ compile.sh
@@ -1,1 +1,22 @@
+#!/bin/sh
+
+# Get today's date
+DATE=`date +%Y-%m-%d`
+echo "compile >> date: $DATE"
+
+# Remove previous resume
+rm ./*Resume*.pdf
+echo "compile >> removed previous resumes"
+
+# Compile new resume
 xelatex resume.tex
+
+# If we successfully made a new resume, rename it
+if [ -e resume.pdf ]
+then
+  echo "compile >> ok"
+  mv resume.pdf "Josh_Hawkins_Resume_$DATE.pdf"
+else
+  echo "compile >> not ok"
+fi
+
